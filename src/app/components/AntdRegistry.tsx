429c45b0b527f4be44dae90c74813d81e837eaaa
五月的夏天 <285258675@qq.com>
2023-11-26 21:53:51 +0800

    feat: 完成项目基本配置

---
+++ src/app/components/AntdRegistry.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from 'react'
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
+import type Entity from '@ant-design/cssinjs/es/Cache'
+import { useServerInsertedHTML } from 'next/navigation'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+
+const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
+  const cache = React.useMemo<Entity>(() => createCache(), [])
+  const isServerInserted = React.useRef<boolean>(false)
+  useServerInsertedHTML(() => {
+    // 避免 css 重复插入
+    if (isServerInserted.current) {
+      return
+    }
+    isServerInserted.current = true
+    return (
+      <style
+        id="antd"
+        dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}
+      />
+    )
+  })
+  return (
+    <StyleProvider cache={cache}>
+      <ConfigProvider locale={zhCN}>{children}</ConfigProvider>
+    </StyleProvider>
+  )
+}
+
+export default StyledComponentsRegistry
