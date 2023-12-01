0eb02ad9f7af4dc0c2438796ddb58f37eb618634
骆沛 <luopei@11.com>
2023-12-01 10:29:21 +0800

    refactor: 调整首页样式和数据库模型

--- src/app/(views)/layout.tsx
+++ src/app/(views)/layout.tsx
@@ -12,7 +12,9 @@ const ViewLayout: React.FC<Props> = ({ children }) => {
       <div className='w-[72px] fixed top-0 bottom-0 left-0'>
         <NavSide></NavSide>
       </div>
-      <div className='flex-1'>{children}</div>
+      <div className='ml-[72px]' style={{ width: 'calc(100% - 72px)' }}>
+        {children}
+      </div>
     </div>
   )
 }
